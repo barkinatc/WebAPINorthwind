525a800c0f9dd2d15436a9342227ff5a2b4a556b
brkna <brkna@BARKIN>
2023-03-27 13:46:39 +0300

    Add project files.

---
+++ WebAPINorthwind/RequestModels/CategoryCreateRequestModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPINorthwind.RequestModels
+{
+    // KULLANICI BİR KATEGORİ EKLEMEK İÇİN NASIL BİR VERİ GÖNDERMELİ BUNU TASARLIYORUZ
+    public class CategoryCreateRequestModel
+    {
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+
+    }
+}
